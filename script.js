7cd7d37e8855d961362deab7c933aff92853032a
Ronaldo Junior <152401171+Dev-RonaldoJunior@users.noreply.github.com>
2024-09-13 15:35:19 -0300

    update geral
    Melhorei a legibilidade dos códigos, deixando-os mais limpos e fáceis de entender. Também apliquei melhorias, como a exibição do volume do som em porcentagem e a troca dos botões de play e pause. Além disso, atualizei e melhorei o conteúdo de cada aba para deixá-lo mais atualizado e aprimorado.

--- script.js
+++ script.js
@@ -12,19 +12,24 @@ menuToggle.addEventListener("click", () => {
 const audio = document.getElementById('background-music');
 const playPauseButton = document.getElementById('play-pause');
 const volumeControl = document.getElementById('volume');
+const volumeLabel = document.getElementById('volume-label'); // Nova label para mostrar a porcentagem
 
 // Função para alternar entre play e pause
 function togglePlayPause() {
     if (audio.paused) {
         audio.play();
-        playPauseButton.textContent = 'Pause';
+        playPauseButton.classList.remove('fa-play');
+        playPauseButton.classList.add('fa-pause');
     } else {
         audio.pause();
-        playPauseButton.textContent = 'Play';
+        playPauseButton.classList.remove('fa-pause');
+        playPauseButton.classList.add('fa-play');
     }
 }
 
-// Atualiza o volume do áudio
+// Atualiza o volume do áudio e mostra a porcentagem
 volumeControl.addEventListener('input', function() {
-    audio.volume = this.value;
+    const volume = this.value;
+    audio.volume = volume;
+    volumeLabel.textContent = `${Math.round(volume * 100)}%`; // Atualiza a porcentagem
 });
